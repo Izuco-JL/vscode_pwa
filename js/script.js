e3bc21019c89312621c1760eb6d346c355b57d6a
Izuco-JL <jordane.lagay@livecampus.tech>
2022-07-01 11:18:38 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* console.log('le script est bien chargé')
+
+// Capter une balise HTML en JavaScript
+const burgerButton = document.querySelector('.navbar-burger')
+const navbarMenu = document.querySelector('.navbar-menu')
+// Ajouter un écouteur d'événement
+burgerButton.addEventListener('click', event => {
+    console.log('clic sur le bouton')
+}) */
